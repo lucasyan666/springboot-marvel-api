3146aaa8c622bbdeaa2d9ba05ef7911a4b6042b5
Lucas <lanxin.yan10@gmail.com>
2022-09-02 21:59:10 +0100

    Migrated getCharacters() and getCharactersByID(int ID) to the new class - MarvelClient

---
+++ src/main/java/com/lucas/marvel/client/MarvelClient.java
@@ -0,0 +1,45 @@
+package com.lucas.marvel.client;
+
+import com.lucas.marvel.models.MarvelResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class MarvelClient {
+
+    private String publicKey = "22a66c451d78a0ae58b99788a7a07da1";
+    private String privateKey = "be0b10a0505455f5bd87b7c621c8b6a84fe470bb";
+    private static final String baseUri = "http://gateway.marvel.com/v1/public/";
+    private int limit = 100;
+    @Autowired
+    RestTemplate restTemplate;
+
+    //GET /v1/public/characters
+    public MarvelResponse getCharacters() {
+        String URI = baseUri + "characters?" + authInfo() + "&limit=" + limit;
+        MarvelResponse res = restTemplate.getForObject(URI, MarvelResponse.class);
+        return res;
+    }
+
+    //GET /v1/public/characters/{characterId}
+    public MarvelResponse getCharacterByID(Integer id) {
+        String URI = baseUri + "characters/" + id + "?" + authInfo();
+        MarvelResponse res = restTemplate.getForObject(URI, MarvelResponse.class);
+        return res;
+    }
+
+    public String authInfo() {
+        StringBuilder str = new StringBuilder();
+        long time = System.currentTimeMillis();
+        str.append("ts=");
+        str.append(time);
+        str.append("&apikey=");
+        str.append(publicKey);
+        str.append("&hash=");
+        str.append(DigestUtils.md5DigestAsHex((time + privateKey + publicKey).getBytes()));
+
+        return str.toString();
+    }
+}
